2b9d425b1d5db1efdb33e8ef36f7baff76c0906a
Didier Fanchon <didier.fanchon@webtales.fr>
2017-01-03 15:21:56 +0100

    Refactoring with data attribute

--- src/AMPCache.php
+++ src/AMPCache.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace AMPCache;
+
 use Alambic\Exception\ConnectorConfig;
 use Goutte\Client;
 
@@ -15,37 +17,37 @@ class AMPCache
     public function __invoke($payload = [])
     {
         // Bypass cache
-        if (isset($payload["pipelineParams"]["noCache"]) && $payload["pipelineParams"]["noCache"]) {
+        if (isset($payload['pipelineParams']['noCache']) && $payload['pipelineParams']['noCache']) {
             return $payload;
         }
         // Get configuration
-        $graphQLAmpApiRoute = isset($payload["connectorBaseConfig"]["graphQLAmpApiRoute"]) ? $payload["connectorBaseConfig"]["graphQLAmpApiRoute"] : null;
-        if (!$graphQLAmpApiRoute){
+        $graphQLAmpApiRoute = isset($payload['connectorBaseConfig']['graphQLAmpApiRoute']) ? $payload['connectorBaseConfig']['graphQLAmpApiRoute'] : null;
+        if (!$graphQLAmpApiRoute) {
             throw new ConnectorConfig('Insufficient configuration : graphQL AMP api route required');
         }
         if (!isset($payload['response'])) {
-            $urlParts = parse_url($_SERVER["HTTP_HOST"]);
-            $host = utf8_encode(isset($urlParts["host"]) ? $urlParts["host"] : $urlParts["path"]);
+            $urlParts = parse_url($_SERVER['HTTP_HOST']);
+            $host = utf8_encode(isset($urlParts['host']) ? $urlParts['host'] : $urlParts['path']);
             //$host = str_replace("-","--",$host);
             //$host = str_replace(".","-",$host);
-            $secure = empty($_SERVER["HTTPS"]) ? false : true;
-            $baseUri = "https://cdn.ampproject.org";
+            $secure = empty($_SERVER['HTTPS']) ? false : true;
+            $baseUri = 'https://cdn.ampproject.org';
             if ($secure) {
-                $uri = $baseUri."/c/s/";
+                $uri = $baseUri.'/c/s/';
             } else {
-                $uri = $baseUri."/c/";
+                $uri = $baseUri.'/c/';
             }
-            $uri.=$host.$graphQLAmpApiRoute."?query=".$payload['pipelineParams']['parentRequestString'];
+            $uri .= $host.$graphQLAmpApiRoute.'?query='.$payload['pipelineParams']['parentRequestString'];
             $client = new Client();
             try {
                 $crawler = $client->request('GET', $uri);
-                $response = json_decode($crawler->filterXPath('//*[@id='data']')->text(), true);
-                $payload['response'] = $response['articles'];
+                $response = json_decode(base64_decode($crawler->filterXPath("//*[@id='data']")->attr('data-response')), true);
+                $payload['response'] = $response[array_keys($response)[0]];
             } catch (RequestException $e) {
                 return $payload;
             }
-
         }
+
         return $payload;
     }
 }
